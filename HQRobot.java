c62c4e13f359aa632edb0a128f43b8ebf8456068
dmayo <dmayo2@mit.edu>
2014-01-07 21:41:40 -0500

    HQ attack

--- HQRobot.java
+++ HQRobot.java
@@ -1,9 +1,7 @@
 package team093;
 
-import battlecode.common.Direction;
-import battlecode.common.GameConstants;
-import battlecode.common.RobotController;
 import battlecode.common.*;
+
 import java.util.*;
 
 public class HQRobot extends BaseRobot {
@@ -13,12 +11,21 @@ public class HQRobot extends BaseRobot {
 	
 	@Override
 	public void run() throws GameActionException {
-			//Check if a robot is spawnable and spawn one if it is
-			if (rc.isActive() && rc.senseRobotCount() < 25) {
+		//Check if a robot is spawnable and spawn one if it is
+		if (rc.isActive()){
+			//spawn more robots
+			if(rc.senseRobotCount() < 25) {
 				Direction toEnemy = rc.getLocation().directionTo(rc.senseEnemyHQLocation());
 				if (rc.senseObjectAtLocation(rc.getLocation().add(toEnemy)) == null) {
 					rc.spawn(toEnemy);
 				}
 			}
+			Robot[] nearbyEnemies = rc.senseNearbyGameObjects(Robot.class,15,rc.getTeam().opponent());
+			if (nearbyEnemies.length > 0) {
+				RobotInfo robotInfo = rc.senseRobotInfo(nearbyEnemies[0]);
+				rc.attackSquare(robotInfo.location);
+			}
+		}
+		
 	}
 }
